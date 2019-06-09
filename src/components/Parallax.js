b073bfecfab56d7c81e2dabe02cf6f51dd076989
bfrest <freston75@gmail.com>
2019-06-09 08:54:22 -0600

    working on the stuff

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,48 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import "./parallax.css";
 
-const Wrap = styled.div`
-  .parallax {
-    height: 100vh;
-    width: 100vw;
-    background-size: cover;
-    background-repeat: no-repeat;
-  }
-
-  .parallax1 {
-    background-image: url("https://images.pexels.com/photos/2376194/pexels-photo-2376194.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500");
-  }
-
-  .parallax2 {
-    background-image: url("https://images.pexels.com/photos/2170473/pexels-photo-2170473.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500");
-  }
-
-  .parallax3 {
-    background-image: url("https://images.pexels.com/photos/2387875/pexels-photo-2387875.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500");
-  }
-`;
+const Wrap = styled.div``;
 
 const Parallax = () => {
   return (
     <Wrap>
-      <div className="parallax parallax1">
-        <div className="content">
-          <h2>hi</h2>
-          <h4>hi</h4>
+      <div className="pimg1">
+        <div className="ptext">
+          <span className="border">Lets Talk</span>
         </div>
       </div>
 
-      <div className="parallax parallax2">
-        <div className="content">
-          <h2>hi</h2>
-          <h4>hi</h4>
+      <section className="section section-light">
+        <iframe width="70%" height="100%" src="https://res.cloudinary.com/bfrest/video/upload/v1559958732/wolfahl/Snow_sundance_official.mp4" />
+      </section>
+
+      <div className="pimg2">
+        <p>hello</p>hi
+      </div>
+
+      <section className="section section-dark">
+        <iframe width="200px" height="100px" src="" />
+      </section>
+
+      <div className="pimg3">
+        <div className="ptext">
+          <span className="border trans">Image Three Text</span>
         </div>
       </div>
 
-      <div className="parallax parallax3">
-        <div className="content">
-          <h2>hi</h2>
-          <h4>hi</h4>
+      <section className="section section-dark">
+        <h2>Section Three</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis
+          consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates
+          inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis
+          voluptatem doloremque fugiat dolor repellendus ratione in.
+        </p>
+      </section>
+
+      <div className="pimg1">
+        <div className="ptext">
+          <span className="border">Lets Talks</span>
         </div>
       </div>
     </Wrap>
