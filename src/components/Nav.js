5ce3811685f915456812b0e89f318a7fd8a5044d
bfrest <freston75@gmail.com>
2019-06-20 21:54:26 -0600

    got the subnav styled and lookin good

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,66 @@
-import React from "react";
-import "./navStyles.css";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Dropdown = styled.div`
+  z-index: 999999;
+  background-color: #222;
+  color: #fff;
+`;
+
+const NavWrap = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-content: center;
+  background-color: #222;
+  color: #fff;
+  height: 90px;
+
+  li {
+    list-style: none;
+    font-size: 1.2em;
+    font-weight: 600;
+    padding: 15px 15px;
+    cursor: pointer;
+  }
+
+  li:hover {
+    color: #888;
+  }
+
+  div {
+    z-index: 1;
+  }
+
+  @media (max-width: 568px) {
+    li {
+      font-size: 0.8em;
+    }
+  }
+`;
 
 const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const showDropdown = () => {
+    showMenu ? setShowMenu(false) : setShowMenu(true);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Galleries ▼</li>
-      </ul>
-    </nav>
+    <NavWrap>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <div>
+        <li onClick={() => showDropdown()}>Categories ▼</li>
+        {showMenu && (
+          <Dropdown>
+            <li>Corporate</li>
+            <li>Promo</li>
+            <li>Weddings</li>
+          </Dropdown>
+        )}
+      </div>
+    </NavWrap>
   );
 };
 
